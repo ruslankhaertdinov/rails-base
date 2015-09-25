3e3bf7ca0b8a8d15af30a5ba2b71824b8e3bef7e
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2015-09-25 22:35:58 +0300

    refactor social_networks/sign_up_spec

--- spec/features/visitor/social_networks/sign_up_spec.rb
+++ spec/features/visitor/social_networks/sign_up_spec.rb
@@ -8,62 +8,57 @@ feature "Sign Up" do
 
   before { stub_omniauth(provider, omniauth_params) }
 
-  context "when provider is Facebook" do
-    let(:provider) { :facebook }
+  context "when user and social link not exist" do
+    before { visit root_path }
 
-    context "when user and social link not exist" do
-      before do
-        visit root_path
-        click_link "Sign in with Facebook"
-      end
+    context "when provider is Google" do
+      let(:provider) { :google_oauth2 }
 
       scenario "Visitor signs up through provider" do
+        click_link "Sign in with Google"
+
         expect(page).to have_content("Signed up successfully.")
         expect(page).to have_text(registered_user.email)
       end
     end
 
-    context "when social link and user exists" do
-      let(:user) { FactoryGirl.create(:user, :confirmed, user_attributes) }
+    context "when provider is Facebook" do
+      let(:provider) { :facebook }
 
-      before do
-        user.social_links.create(provider: provider, uid: uid)
-        visit root_path
+      scenario "Visitor signs up through provider" do
         click_link "Sign in with Facebook"
-      end
 
-      scenario "Visitor signs in through provider" do
-        expect(page).to have_content("Signed in successfully.")
+        expect(page).to have_content("Signed up successfully.")
         expect(page).to have_text(registered_user.email)
       end
     end
   end
 
-  context "when provider is Google" do
-    let(:provider) { :google_oauth2 }
+  context "when social link and user exists" do
+    let(:user) { create(:user, :confirmed, user_attributes) }
 
-    context "when user and social link not exist" do
-      before do
-        visit root_path
+    before do
+      user.social_links.create(provider: provider, uid: uid)
+      visit root_path
+    end
+
+    context "when provider is Google" do
+      let(:provider) { :google_oauth2 }
+
+      scenario "Visitor signs in through provider" do
         click_link "Sign in with Google"
-      end
 
-      scenario "Visitor signs up through provider" do
-        expect(page).to have_content("Signed up successfully.")
+        expect(page).to have_content("Signed in successfully.")
         expect(page).to have_text(registered_user.email)
       end
     end
 
-    context "when social link and user exists" do
-      let(:user) { FactoryGirl.create(:user, :confirmed, user_attributes) }
-
-      before do
-        user.social_links.create(provider: provider, uid: uid)
-        visit root_path
-        click_link "Sign in with Google"
-      end
+    context "when provider is Facebook" do
+      let(:provider) { :facebook }
 
       scenario "Visitor signs in through provider" do
+        click_link "Sign in with Facebook"
+
         expect(page).to have_content("Signed in successfully.")
         expect(page).to have_text(registered_user.email)
       end
