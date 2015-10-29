21df92dfeed903359f0af29306846ee8f9f713a1
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2015-10-29 23:01:13 +0300

    add debugging output

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,8 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     super
     session[:omniauth] = nil unless @user.new_record?
+  rescue => e
+    puts "*********************Error: #{e}"
   end
 
   private
